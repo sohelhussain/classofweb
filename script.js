e1497e3104776c08ea2cf9c6e5283c71082c45a6
sohelhussain <sohelhussaing@gmail.com>
2023-11-04 15:57:05 -0700

    hello

---
+++ script.js
@@ -0,0 +1,11 @@
+for(var i = 0; i<11; i++){
+    console.log("hii")
+}
+
+// var naam = prompt("give a password(the password is now)");
+// console.log(naam);
+// while(naam != "now"){
+//     naam = prompt("type a password");
+//     console.log(naam);
+// }
+
