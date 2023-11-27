59ecccfee8aa68fee3362c7a1621d5a9b6acf290
sohelhussain <sohelhussaing@gmail.com>
2023-11-26 16:21:39 -0800

    custom curesur

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const arr = ["box", "circle", "try"];
+let cursor = document.querySelector("#cursor")
 
-document.querySelector(`button`).addEventListener("click",() => {
-  const num = Math.floor(Math.random() * arr.length);
-  // console.log(arr[num]);
-  document.querySelector(".box").classList.add(arr[num]);
+document.querySelector('#main').addEventListener("mousemove",(detes) => {
+  cursor.style.left = detes.x + "px"
+  cursor.style.top = detes.y + "px"
+})
+document.querySelector(`h1`).addEventListener("mouseenter",() => {
+  cursor.style.scale = 2
+})
+document.querySelector(`h1`).addEventListener("mouseleave",() => {
+  cursor.style.scale = 1
 })
