13d0ee30d3d9c3b94a6eea927eb73344bfae7334
sohelhussain <sohelhussaing@gmail.com>
2023-11-28 11:04:21 -0800

    box moving with keyborde events

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let cursor = document.querySelector("#cursor")
-
-document.querySelector('#main').addEventListener("mousemove",(detes) => {
-  cursor.style.left = detes.x + "px"
-  cursor.style.top = detes.y + "px"
-})
-document.querySelector(`h1`).addEventListener("mouseenter",() => {
-  cursor.style.scale = 2
-})
-document.querySelector(`h1`).addEventListener("mouseleave",() => {
-  cursor.style.scale = 1
+// let box = document.querySelectorAll(`.box`)
+// let arr = ["box-selected","box-selected2","box-selected3"]
+// let num = Math.floor(Math.random()*10)
+// box.forEach((dets)=> {
+//   dets.addEventListener("click", (e)=> {
+//     dets.classList.add(arr[num])
+//   })
+// })
+let grow = 0
+let growVar = 0;
+window.addEventListener("keydown", (e)=> {
+  console.log(e);
+  if (e.key == "ArrowRight") {
+    grow += 10
+    document.querySelector(".box").style.left = grow + "px";
+  }
+  else if (e.key == "ArrowLeft") {
+    grow -= 10
+    document.querySelector(".box").style.left = grow + "px";
+  }
+  else if (e.key == "ArrowUp") {
+    growVar -= 10
+    document.querySelector(".box").style.top = growVar + "px";
+  }
+  else if (e.key == "ArrowDown") {
+    console.log(e);
+    growVar += 10
+    document.querySelector(".box").style.top = growVar + "px";
+  }
+  
 })
