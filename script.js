5efa7c5333194b163a9a030b3712458e4d2bd8fb
sohelhussain <sohelhussaing@gmail.com>
2024-01-12 21:18:16 -0800

    locomotive js

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-// let box = document.querySelectorAll(`.box`)
-// let arr = ["box-selected","box-selected2","box-selected3"]
-// let num = Math.floor(Math.random()*10)
-// box.forEach((dets)=> {
-//   dets.addEventListener("click", (e)=> {
-//     dets.classList.add(arr[num])
-//   })
-// })
-let grow = 0
-let growVar = 0;
-window.addEventListener("keydown", (e)=> {
-  console.log(e);
-  if (e.key == "ArrowRight") {
-    grow += 10
-    document.querySelector(".box").style.left = grow + "px";
+const fnc = () =>{
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+};
+fnc();
+gsap.from("img",{
+  scale:0,
+  scrollTrigger: {
+    trigger: "#container-two",
+    scroller:"#main",
+    start:"top 30%",
+    end:"bottom 30%",
+    markers: true,
+    scrub:1
   }
-  else if (e.key == "ArrowLeft") {
-    grow -= 10
-    document.querySelector(".box").style.left = grow + "px";
-  }
-  else if (e.key == "ArrowUp") {
-    growVar -= 10
-    document.querySelector(".box").style.top = growVar + "px";
-  }
-  else if (e.key == "ArrowDown") {
-    console.log(e);
-    growVar += 10
-    document.querySelector(".box").style.top = growVar + "px";
-  }
-  
 })
