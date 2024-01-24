953de2a018484d8d8b12c62a68fd3cd87a8b5aec
sohelhussain <sohelhussaing@gmail.com>
2024-01-23 17:01:27 -0800

    swiper

--- script.js
+++ script.js
@@ -34,14 +34,20 @@ ScrollTrigger.refresh();
 
 };
 fnc();
-gsap.from("img",{
-  scale:0,
-  scrollTrigger: {
-    trigger: "#container-two",
-    scroller:"#main",
-    start:"top 30%",
-    end:"bottom 30%",
-    markers: true,
-    scrub:1
-  }
-})
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
